8fdb3c6c7c44b733edcdd27fb9c4b03b6b78f7f0
Jehr_ <jehrman27@gmail.com>
2023-08-14 14:12:38 -0400

    Added campground validations and error handling throughout site

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const {campgroundSchema} = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -26,63 +29,95 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    // validation campground data against schema and grab any errors
+    const {error} = campgroundSchema.validate(req.body);
+
+    // throw errors, otherwise go to next middleware
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+
+}
+
 // root route
 app.get('/', (req, res) =>{
     res.render('home');
-})
+});
 
 // route to show index of campgrounds
-app.get('/campgrounds', async (req, res) =>{
+app.get('/campgrounds', catchAsync(async (req, res) =>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+}));
 
 // page for creating new campgrounds
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
-})
+});
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // catch if campground data is incomplete
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+
+    // grab campground data from request body
     const campground = new Campground(req.body.campground);
+
+    // save campground data to db and then redirect to page for that campground
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
 // route for individual campgrounds
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     // get campground by database id
     const campground = await Campground.findById(req.params.id);
     // route to campground view and pass in campground obj
     res.render('campgrounds/show', { campground });
-})
+}));
 
 // page for editing existing campgrounds
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     // get campground by database id
     const campground = await Campground.findById(req.params.id);
     // route to campground edit view and pass in campground obj
     res.render('campgrounds/edit', { campground });
-})
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     // destructure id from request parameters
     const {id} = req.params;
     // find campground by id and update by spreading campground from req
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     // redirect to page for update campground
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     // destructure id from request parameters
     const {id} = req.params;
     // find and delete campground from db
     await Campground.findByIdAndDelete(id);
     // redirect to campgrounds index page
     res.redirect('/campgrounds');
-})
+}));
+
+// catch all other urls with a 404 and send to error middleware
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oops! Something went wrong.'
+    res.status(statusCode).render('error', {err});
+});
 
 // open connection at localhost:3000
 app.listen(3000, ()=> {
     console.log('Serving on port 3000');
-})
+});
---
+++ schemas.js
@@ -0,0 +1,14 @@
+// This file contains the schemas for joi validations
+
+const Joi = require('joi');
+
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
+
